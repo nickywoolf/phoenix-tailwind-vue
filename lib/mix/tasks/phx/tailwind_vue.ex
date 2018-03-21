4911f496bd6547aeedad264d48db925f92641e15
Nicky Woolf <woolfnicky@gmail.com>
2018-03-21 07:35:06 -0700

    Create mix task

---
+++ lib/mix/tasks/phx/tailwind_vue.ex
@@ -0,0 +1,11 @@
+defmodule Mix.Tasks.Phx.TailwindVue do
+  @moduledoc """
+  Installs Tailwindcss and Vue in a Phoenix project.
+  """
+
+  use Mix.Task
+
+  @shortdoc "Installs Tailwindcss and Vue in a Phoenix project."
+  def run(_) do
+  end
+end
