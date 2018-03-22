81d985cf01e4826ab308e0cd2bd4d5e0e1f08835
Nicky Woolf <woolfnicky@gmail.com>
2018-03-21 19:20:46 -0700

    Setup assets in phoenix project

--- lib/mix/tasks/phx/tailwind_vue.ex
+++ lib/mix/tasks/phx/tailwind_vue.ex
@@ -6,6 +6,38 @@ defmodule Mix.Tasks.Phx.TailwindVue do
   use Mix.Task
 
   @shortdoc "Installs Tailwindcss and Vue in a Phoenix project."
-  def run(_) do
+
+  @directories [
+    "assets/css",
+    "assets/js/components"
+  ]
+
+  @assets [
+    {"assets/.babelrc", ".bablerc"},
+    {"assets/package.json", "package.json"},
+    {"assets/postcss.config.js", "postcss.config.js"},
+    {"assets/webpack.config.js", "webpack.config.js"},
+    {"assets/css/app.css", "assets/css/app.css"},
+    {"assets/js/app.js", "assets/js/app.js"},
+    {"assets/js/components/Hello.vue", "assets/js/components/Hello.vue"}
+  ]
+
+  def run(_args) do
+    Enum.each(@directories, &make_directory/1)
+    Enum.each(@assets, &copy_file/1)
+    System.cmd("npm", ["install"])
+    System.cmd(Path.expand("./node_modules/.bin/tailwind"), ["init"])
+  end
+
+  defp make_directory(dir) do
+    dir |> Path.expand() |> File.mkdir_p!()
+  end
+
+  defp copy_file({file, destination}) do
+    file |> local_path() |> File.copy!(Path.expand(destination))
+  end
+
+  defp local_path(file) do
+    Path.expand("../../../../" <> file, unquote(__DIR__))
   end
 end
