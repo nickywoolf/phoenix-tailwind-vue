dab09a210f614b9ce87744cb3375955c54dce8b2
Nicky Woolf <woolfnicky@gmail.com>
2018-05-01 06:40:26 -0700

    Register component globally

--- template/assets/js/app.js
+++ template/assets/js/app.js
@@ -2,9 +2,8 @@ import style from '../css/app.css'
 
 import Vue from 'vue'
 
+Vue.component('hello', require('./components/Hello.vue').default)
+
 const app = new Vue({
   el: '#app',
-  components: {
-    'hello': require('./components/Hello.vue').default
-  }
 })
