e4fdf93b5adfebce334c76d699c87bc7769a27af
Nicky Woolf <woolfnicky@gmail.com>
2018-10-21 08:30:20 -0700

    Get purge css working

--- template/webpack.mix.js
+++ template/webpack.mix.js
@@ -7,10 +7,14 @@ mix.js("assets/js/app.js", "priv/static/js")
     .postCss("assets/css/app.css", "priv/static/css")
     .tailwind()
     .purgeCss({
-        extensions: ["eex"],
-        folders: ["lib/tailwave_web/templates"]
+        enabled: mix.inProduction(),
+        globs: [
+            path.join(__dirname, "assets/js/components/*.vue"),
+            path.join(__dirname, "lib/*/templates/*/*.eex")
+        ],
+        extensions: ["eex", "vue"],
     })
 
 if (mix.inProduction()) {
-    mix.version()
+    // mix.version()
 }
