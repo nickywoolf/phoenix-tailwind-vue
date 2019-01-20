b41481f211bb480d6f025daf12f4e1ad050adba6
Nicky Woolf <woolfnicky@gmail.com>
2019-01-19 16:54:59 -0800

    Moves package.json and webpack config to assets directory

--- template/assets/webpack.mix.js
+++ template/assets/webpack.mix.js
@@ -3,15 +3,15 @@ const mix = require("laravel-mix")
 require("laravel-mix-tailwind")
 require("laravel-mix-purgecss")
 
-mix.setPublicPath(path.resolve('./'))
-    .js("assets/js/app.js", "priv/static/js")
-    .postCss("assets/css/app.css", "priv/static/css")
+mix.setPublicPath(path.resolve('./priv/static'))
+    .js("js/app.js", "js")
+    .postCss("css/app.css", "css")
     .tailwind()
     .purgeCss({
         enabled: mix.inProduction(),
         globs: [
-            path.join(__dirname, "assets/js/components/*.vue"),
-            path.join(__dirname, "lib/*/templates/*/*.eex")
+            path.join(__dirname, "js/components/*.vue"),
+            path.join(__dirname, "../lib/*/templates/*/*.eex")
         ],
         extensions: ["eex", "vue"],
     })
